66603f9fec88f6cede48588cb63cf3b2fe65dda9
HeartvsFeather <104656103+HeartvsFeather@users.noreply.github.com>
2024-04-10 16:20:44 +0300

    Update second_problem.py
    Add arrow feature

--- second_problem.py
+++ second_problem.py
@@ -7,16 +7,12 @@ def f(x): # f(x)
 def df(x): # derivative of f(x)
     return -3 * np.sin(3 * x) + 0.2
 
-x_plt = np.arange(-5.0, 5.0, 0.05) # x's with 0.05 step
+x_plt = np.arange(-5.0, 5.0, 0.01) # x's with 0.05 step
 f_plt = [f(x) for x in x_plt] # y(x)'s
+df_plt = [df(x) for x in x_plt] # dy(x)'s
 
-fig, ax = plot.subplots() # built graph of f(x)
-ax.grid(True)
-ax.plot(x_plt, f_plt)
-plot.show()
-
-df_plt = [df(x) for x in x_plt] # built graph of df(x) with feathers
-get_ipython().run_line_magic('matplotlib', 'notebook')
+# built graph of df(x) with feature
+get_ipython().run_line_magic('matplotlib', 'notebook') #feature
 fig, ax = plot.subplots()
 ax.grid(True)
 ax.plot(x_plt, df_plt)
@@ -31,10 +27,40 @@ lr = 1 # length of first step
 # definition of colmns
 print("Длина шага | Икс | Игрек от икса| Игрек производной от икса|\n")
 
-for i in range(1, 1000): # x = x - df(x_null)
+for i in range(1, N): # x = x - df(x_null)
     lr = 1 / i # length of i step
+    
     x = x - lr * np.sign(df(x)) # step to right or to left with lr length step
     
     print(f"{'{:.4f}'.format(lr)} | {'{:.4f}'.format(x)} | {'{:.4f}'.format(f(x))} | {'{:.4f}'.format(df(x))}")
+
+res = x
+
+x = x_null
+
+# built graph of f(x) with feature
+fig, ax = plot.subplots()
+ax.grid(True)
+ax.plot(x_plt, f_plt)
+
+for i in range(1, N):
+    lr = 0.05 # length of step if need careful arrows on graph
     
-print(f"\nСпуск градиента нас привел к от точки с координатами ({'{:.2f}'.format(x_null)}, {'{:.2f}'.format(f(x_null))}) к точке c координатами ({'{:.2f}'.format(x)}, {'{:.4f}'.format(f(x))})")
+    x_prev = x
+    y_prev = f(x)
+    
+    if x_prev >= x:
+        dx = -(x_prev - x)
+    else:
+        dx = x - x_prev
+    if y_prev >= f(x):
+        dy = -(y_prev - f(x))
+    else:
+        dy = f(x) - y_prev
+    plot.arrow(x = x_prev, y = y_prev, dx = dx, dy = dy, width= .03)
+    
+    x = x - lr * np.sign(df(x))
+    
+print(f"\nСпуск градиента нас привел к от точки с координатами ({'{:.2f}'.format(x_null)}, {'{:.2f}'.format(f(x_null))}) к точке c координатами ({'{:.2f}'.format(res)}, {'{:.2f}'.format(f(res))})")
+
+plot.show() # graph with arrows
