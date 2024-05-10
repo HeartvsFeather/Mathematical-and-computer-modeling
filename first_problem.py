e6416ae468413c2aa11699b6e73c1f5edd1fef1f
hvf <albus.eshman@yandex.ru>
2024-05-10 19:39:08 +0300

    Rework first problem.

--- first_problem.py
+++ first_problem.py
@@ -1,66 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-def f(x): # f(x)
-    return np.cos(3 * x) + 0.2 * x
+class Main:
+    def gradient_descent(self):
+        # ввод начального положения
+        try:
+            x_null = float(input("Введите начальную точку в формате 0.00 от -5 до 5: "))
+            if x_null < -5.0 or x_null > 5.0:
+                print("\nНачальное положение может быть только в диапозоне от -5 до 5")
+                input("Enter для закрытия")
+                return
+        except ValueError:
+            print("\nНекорректно введено начальное положение")
+            input("Enter для закрытия")
+            return
 
-def df(x): # derivative of f(x)
-    return -3 * np.sin(3 * x) + 0.2
+        def f(x): # возвращает значения f(x)
+            return np.cos(3 * x) + 0.2 * x
 
-x_plt = np.arange(-5.0, 5.0, 0.01) # x's with 0.05 step
-f_plt = [f(x) for x in x_plt] # y(x)'s
-df_plt = [df(x) for x in x_plt] # dy(x)'s
+        def df(x): # возвращает значения f'(x)
+            return -3 * np.sin(3 * x) + 0.2
 
-# built graph of df(x) with feature
-get_ipython().run_line_magic('matplotlib', 'notebook') #feature
-fig, ax = plot.subplots()
-ax.grid(True)
-ax.plot(x_plt, df_plt)
-plot.show()
+        # значения x и f(x) с шагом 0.01 (*определяет точность решения) от -5 до 5
+        x_plt = np.arange(-5.0, 5.0, 0.01) # x
+        f_plt = [f(x) for x in x_plt] # f(x)
 
-x_null = 0 # at this point start grad descent
-y = f(x_null) # y(x_null)
-x = x_null
-N = 1000 # amount of steps
-lr = 1 # length of first step
+        N = 1000 # количество шагов
+        lr = 0.01 # длина шага смещения
 
-# definition of colmns
-print("Длина шага | Икс | Игрек от икса| Игрек производной от икса|\n")
+        # параметры для отображения графика
+        fig, ax = plot.subplots()
+        ax.grid(True)
+        ax.plot(x_plt, f_plt) # f(x) на графике
 
-for i in range(1, N): # x = x - df(x_null)
-    lr = 1 / i # length of i step
-    
-    x = x - lr * np.sign(df(x)) # step to right or to left with lr length step
-    
-    print(f"{'{:.4f}'.format(lr)} | {'{:.4f}'.format(x)} | {'{:.4f}'.format(f(x))} | {'{:.4f}'.format(df(x))}")
+        # проверка на изначальное попадание в экстремум
+        if df(x_null) == 0:
+            x -= 0.01
+        else:
+            x = x_null
 
-res = x
+        # вычисление x и f(x) после спуска по принципу x = x - f'(x)
+        for i in range(N):
+            # перменные для отображения стрелок
+            x_prev = x
+            y_prev = f(x)
 
-x = x_null
+            x = x - lr * np.sign(df(x)) # шаг с длиной lr вправо или влево в зависимости
+                                        # от знака производной
 
-# built graph of f(x) with feature
-fig, ax = plot.subplots()
-ax.grid(True)
-ax.plot(x_plt, f_plt)
+            # добавление стрелки на график
+            if x_prev >= x:
+                dx = -(x_prev - x)
+            else:
+                dx = x - x_prev
+            if y_prev >= f(x):
+                dy = -(y_prev - f(x))
+            else:
+                dy = f(x) - y_prev
+            plot.arrow(x = x_prev, y = y_prev, dx = dx, dy = dy, width= 0.02)
 
-for i in range(1, N):
-    lr = 0.05 # length of step if need careful arrows on graph
-    
-    x_prev = x
-    y_prev = f(x)
-    
-    if x_prev >= x:
-        dx = -(x_prev - x)
-    else:
-        dx = x - x_prev
-    if y_prev >= f(x):
-        dy = -(y_prev - f(x))
-    else:
-        dy = f(x) - y_prev
-    plot.arrow(x = x_prev, y = y_prev, dx = dx, dy = dy, width= .03)
-    
-    x = x - lr * np.sign(df(x))
-    
-print(f"\nСпуск градиента нас привел к от точки с координатами ({'{:.2f}'.format(x_null)}, {'{:.2f}'.format(f(x_null))}) к точке c координатами ({'{:.2f}'.format(res)}, {'{:.2f}'.format(f(res))})")
+            # проверка на зацикленность возле точки минимума
+            if np.sign(df(x)) != np.sign(df(x_prev)):
+                    break
 
-plot.show() # graph with arrows
+        # вывод полученного результата
+        print(f"\nГрадиентный спуск нас привел от точки с координатами "
+              f"({'{:.2f}'.format(x_null)}, {'{:.2f}'.format(f(x_null))}) к точке c "
+              f"координатами ({'{:.2f}'.format(x)}, {'{:.2f}'.format(f(x))}) на "
+              f"функции cos(3x) + 0.2x (погрешность +- 0.01)")
+
+        plot.show() # отображение графика
+
+        input("\nEnter для закрытия")
+        return
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.gradient_descent()
